8d5766a6d5cc798356cd96ac87064763a59b8b0d
yoojaewon <yjw970710@gmail.com>
2024-03-11 20:23:44 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,17 +1,12 @@
 class Solution {
     public boolean solution(int x) {
-        String str = Integer.toString(x);
-        char[] buf = new char[str.length()];
-        int sum = 0;
-        for(int i = 0; i < str.length(); i++){
-            char c = str.charAt(i);
-            buf[i] = c;
+        int num = 0;
+        int buf = x;
+        
+        while(buf > 0){
+            num += buf % 10;
+            buf /= 10;
         }
-        for(int i = 0; i< buf.length; i++){
-            sum += buf[i] - '0';
-        }
-        System.out.println(sum);
-        if (x % sum == 0) return true;
-        else return false;
+        return (x % num == 0) ? true : false;
     }
 }
