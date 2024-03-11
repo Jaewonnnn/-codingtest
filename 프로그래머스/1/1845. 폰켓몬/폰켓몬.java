f2d879c6f87af42d716949110d76964e898928e4
yoojaewon <yjw970710@gmail.com>
2024-03-11 17:57:12 +0900

    [level 1] Title: 폰켓몬, Time: 2.72 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,21 +1,12 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 1;
-        Map<Integer, Integer> map = new HashMap<>();
-        int idx = 1;
-        map.put(idx, nums[0]);
-        for(int i = 0; i < nums.length; i++){
-            if(answer == nums.length/2) break;
-            
-            if(map.containsValue(nums[i])){
-                continue;
-            }
-            else{
-                map.put(++idx, nums[i]);
-                answer++;
-            }
+        Set<Integer> set = new HashSet<>();
+        
+        for(int number : nums){
+            set.add(number);
         }
-        return answer;
+        
+        return (nums.length / 2 <= set.size()) ? nums.length / 2 : set.size();
     }
 }
