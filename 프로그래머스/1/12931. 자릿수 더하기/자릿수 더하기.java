425ded3f69ed8b700511ad4ac99c0b5b1618ce8c
yoojaewon <yjw970710@gmail.com>
2024-03-11 18:44:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        while(n > 0){
+        while(n >= 1){
             answer += n % 10;
             n /= 10;
         }
+
         return answer;
     }
 }
