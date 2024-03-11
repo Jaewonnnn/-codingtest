8ce5e06df496f4abdf796b75aa3f8a6c82270c9e
yoojaewon <yjw970710@gmail.com>
2024-03-11 18:20:27 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.40 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,9 +1,9 @@
 class Solution {
     public String solution(int n) {
         String answer = "";
-        for(int i = 0; i<n; i++){
-            if(i%2==0) answer += "수";
-            else if (i % 2 == 1) answer += "박";
+        for(int i = 1; i <= n; i++){
+            if(i % 2 == 1) answer += "수";
+            else answer += "박";
         }
         return answer;
     }
