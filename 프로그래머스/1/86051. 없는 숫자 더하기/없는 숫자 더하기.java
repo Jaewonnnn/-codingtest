42b380a22c417c7ebfbf6a32496bccf3d5d1d0bf
yoojaewon <yjw970710@gmail.com>
2024-03-19 15:40:48 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -2,9 +2,11 @@ class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
         int[] buf = new int[10];
+        
         for(int i = 0; i < numbers.length; i++){
             buf[numbers[i]]++;
         }
+        
         for(int i = 0; i<buf.length; i++){
             if(buf[i] == 0){
                 answer += i;
