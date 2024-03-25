373cc3c7699b7b9104073be2987e5041f1a1ceb1
yoojaewon <yjw970710@gmail.com>
2024-03-25 17:32:10 +0900

    [level 2] Title: 하노이의 탑, Time: 3.54 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,18 +1,18 @@
 import java.util.*;
 class Solution {
-    public ArrayList<int[]> solution(int n) {
-        ArrayList<int[]> answer = new ArrayList<>();
+    public List<int[]> solution(int n) {
+        List<int[]> answer = new ArrayList<>();
         hanoi(n, 1, 2, 3, answer);
         return answer;
     }
-
-    public void hanoi(int n, int start, int temp, int end, ArrayList<int[]> answer) {
-        if (n == 1) {
+    public void hanoi(int n, int start, int temp, int end, List<int[]> answer){
+        if(n == 1){
             answer.add(new int[]{start, end});
-        } else {
-            hanoi(n - 1, start, end, temp, answer);
+        }
+        else{
+            hanoi(n-1, start, end, temp, answer);
             answer.add(new int[]{start, end});
-            hanoi(n - 1, temp, start, end, answer);
+            hanoi(n-1, temp, start, end, answer);
         }
     }
 }
